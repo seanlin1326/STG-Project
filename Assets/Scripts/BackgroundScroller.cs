eeadba06c6f7605ae0037b89d1f720a38536a058
Sean Lin <seanlin1326@gmail.com>
2022-05-16 21:13:19 +0800

    後處理 子彈製作

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -1,24 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BackgroundScroller : MonoBehaviour
+namespace Sean
 {
-   [SerializeField]Vector2 scrollVelocity;
-    Material material;
-    private void Awake()
+    public class BackgroundScroller : MonoBehaviour
     {
-        material = GetComponent<Renderer>().material;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        [SerializeField] Vector2 scrollVelocity;
+        Material material;
+        private void Awake()
+        {
+            material = GetComponent<Renderer>().material;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        material.mainTextureOffset += scrollVelocity * Time.deltaTime;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            material.mainTextureOffset += scrollVelocity * Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -3,55 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
-
-[CreateAssetMenu(fileName = "Player Input")]
-public class PlayerInput : ScriptableObject,InputActions.IGamePlayActions
+namespace Sean
 {
-    public event UnityAction<Vector2> onMove;
-    public event UnityAction onStopMove;
-    InputActions inputActions;
+    [CreateAssetMenu(fileName = "Player Input")]
+    public class PlayerInput : ScriptableObject, InputActions.IGamePlayActions
+    {
+        public event UnityAction<Vector2> onMove;
+        public event UnityAction onStopMove;
+        InputActions inputActions;
 
-    private void OnEnable()
-    {
-        inputActions = new InputActions();
-        inputActions.GamePlay.SetCallbacks(this);
-    }
-    private void OnDisable()
-    {
-        
-    }
-    public void DisableAllInputs()
-    {
-        inputActions.GamePlay.Disable();
-    }
-    public void EnableGamePlayInput()
-    {
-        inputActions.GamePlay.Enable();
-
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-    public void OnMove(InputAction.CallbackContext context)
-    {
-     if(context.phase == InputActionPhase.Performed)
+        private void OnEnable()
         {
-            onMove?.Invoke(context.ReadValue<Vector2>());
+            inputActions = new InputActions();
+            inputActions.GamePlay.SetCallbacks(this);
         }
-     if(context.phase == InputActionPhase.Canceled)
+        private void OnDisable()
         {
-            onStopMove?.Invoke();
+
         }
-    }
+        public void DisableAllInputs()
+        {
+            inputActions.GamePlay.Disable();
+        }
+        public void EnableGamePlayInput()
+        {
+            inputActions.GamePlay.Enable();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed)
+            {
+                onMove?.Invoke(context.ReadValue<Vector2>());
+            }
+            if (context.phase == InputActionPhase.Canceled)
+            {
+                onStopMove?.Invoke();
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
